4992a12368b43d5c9ab943c77af09b407109325b
Felix Wang <grimmi75380@gmail.com>
2018-03-29 16:03:43 +0800

    Initial commit

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="7c82df6f-4ef6-4dd4-b0a9-f30419955ca6")
+public class DataBindingInfo {}
